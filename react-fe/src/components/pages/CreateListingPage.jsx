799a1e34fd2c9051de23bdee0544bb77f13741a2
David Wu <davidw7@students.cs.ubc.ca>
2021-07-27 20:35:39 -0700

    implemented with Justin's help

--- react-fe/src/components/pages/CreateListingPage.jsx
+++ react-fe/src/components/pages/CreateListingPage.jsx
@@ -1,13 +1,56 @@
 import { withFirebase } from '../Firebase';
 import Header from "../organisms/Header";
 import ListingForm from '../organisms/ListingForm';
+import axios from "axios";
+import {useEffect, useState} from "react";
+import config from "../../config";
 
 const CreateListingPage = (props) => {
-    // TODO: Pass backend call onSubmit fn to ListingForm
+    const url = config.api.url
+    const [authUserHeaders, setAuthUserHeaders] = useState(null);
+
+    useEffect(() => {
+        // Sets authed user when firebase loads current user
+        props.firebase.auth.onAuthStateChanged(authUser => {
+            if (authUser) {
+                authUser.getIdToken().then((token) => {
+                    const reqHeaders = {
+                        headers: {
+                            'Authorization': `Bearer ${token}`
+                        }
+                    };
+                    setAuthUserHeaders(reqHeaders);
+                });
+            } else {
+                setAuthUserHeaders(null);
+            }
+        });
+    }, [props.firebase.auth, url]);
+
+    const createListing = (startDate, endDate, imgUrl, numberAvail, location, dayPrice, instructions, type, size) => {
+        axios.post(url + "/listings/", {
+            startDate: startDate.toDateString(),
+            endDate: endDate.toDateString(),
+            imgUrl,
+            numberAvail,
+            size,
+            location,
+            dayPrice,
+            instructions,
+            type,
+        }, authUserHeaders).then(() => {
+            alert("Successfully Created Listing");
+        }).catch(error => {
+            alert("Server error - Failed to create");
+        });
+    };
+
     return (
         <div>
             <Header />
-            <ListingForm />
+            <ListingForm
+                onSubmit={createListing}
+            />
         </div>
     );
 };
